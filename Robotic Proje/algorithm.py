a0828f9d5e337b529c2ff09402016fd5bb8b1281
Göksu Eldemir <55974337+eldemir18@users.noreply.github.com>
2023-01-13 22:58:38 +0300

    Add files via upload

--- Robotic Proje/algorithm.py
+++ Robotic Proje/algorithm.py
@@ -3,70 +3,77 @@ import cv2
 import numpy as np
 
 # Constants for equilateral shape
-DRONE_COUNT = 40
+DRONE_COUNT = 18
 LENGTH = 10
 
 # Put img path here
-IMAGE = cv2.imread('flag.png')
+IMAGE = cv2.imread('heart.png')
 
-def get_equilateral_corners(drone_count, length):
+def get_equil_corners(drone_count, length):
 
     # Calculating angles
-    angles = np.linspace(0, 2 * np.pi, drone_count, endpoint=False)
+    angles = np.linspace(0, 
+                         2 * np.pi, 
+                         drone_count, 
+                         endpoint=False)
 
-    # Calculating points
+    # Calculating corner coordinate
     x = length * np.cos(angles)
     y = length * np.sin(angles)
 
-    # Merge points
+    # Merge coordinate
     corners = np.column_stack((x, y))
 
     # For visualization can commented out
-    x, y = zip(*corners)
-    plt.scatter(x, y)
+    x, y = zip(*corners[:,:])
+    plt.scatter(x, y, c='r')
+    plt.axhline(0, color='black', lw=0.2)
+    plt.axvline(0, color='black', lw=0.2)
     plt.show()
-
+    
+    # Return corner cordinates
     return corners
 
 def get_image_corners(img, drone_count):
-    if img is None:
-        print("Image not found or not in the correct format")
-        return    
 
-    # Make Image Gray
+    # Make image gray
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
-    # Smoothing Image
+    # Smoothing image
     blur = cv2.GaussianBlur(gray, (5,5), 1)
-
-    # Find Corners
+    
+    # Find corner coordinates
     corners = cv2.goodFeaturesToTrack(blur, 
                                       qualityLevel=0.001, 
                                       minDistance=5,
                                       corners=drone_count, 
                                       maxCorners=drone_count, 
-                                      mask=None)
-    corners = corners.astype(int)
+                                      mask=None
+                                      ).astype(int
+                                      ).reshape(drone_count,2)
 
-    # Normalizing corners
-    corners = normalizeImage(corners, img.shape[0], img.shape[1])
+    # Normalize corner coordinates to origin
+    corners = normalizeCorners(corners, 
+                               img.shape[0], 
+                               img.shape[1])
 
     # For visualization can commented out
-    x, y = zip(*corners[:,0,:])
-    plt.scatter(x, y)
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
+    x, y = zip(*corners[:,:])
+    plt.scatter(x, y, c='r')
+    plt.axhline(0, color='black', lw=0.2)
+    plt.axvline(0, color='black', lw=0.2)
     plt.show()
 
-    # Return corners
+    # Return Corner Coordinates
     return corners
 
-def normalizeImage(corners, H, W):
-    corners[:, 0, 0] -= W // 2
-    corners[:, 0, 1] -= H // 2
+def normalizeCorners(corners, H, W):
+    corners[:,0] -= W // 2
+    corners[:,1] -= H // 2
+    corners[:,1] = -corners[:,1]
     return corners
 
-#get_equilateral_corners(DRONE_COUNT, LENGTH)
+#get_equil_corners(DRONE_COUNT, LENGTH)
 get_image_corners(IMAGE, DRONE_COUNT)
 
     
